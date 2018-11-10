fb049ebfc61da0ac03ca8a88501b5b9a40d4a66e
Kevin Liu <kevinliu314@gmail.com>
2018-11-10 15:07:33 -0600

    modify main file for testign

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from mine import mine
 from spaceship import spaceship
 from board import board
+import utils
 
-input = [['.', 'c', '.'], ['c', '.', 'c' ], ['.', 'c', '.']]
+field_file = utils.getFile('field_file.txt')
+script_file = utils.getFile('script_file.txt')
+input_board = []
+for line in field_file:
+    input_board.append(list(line))
 
-my_board = board(input)
+my_board = board(input_board)
+
+print('Starting Board')
+my_board.printBoard()
