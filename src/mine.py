6a3469bf7de84674d5b7746dbfa9dfaa806e19af
Kevin Liu <kevinliu314@gmail.com>
2018-11-10 12:17:15 -0600

    define mine class

---
+++ src/mine.py
@@ -0,0 +1,6 @@
+class mine:
+    def __init__(self, x_coord, y_coord, z_dist):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.dist = z_dist
+        self.is_alive = True
