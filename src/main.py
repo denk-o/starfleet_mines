9d3b9ca485d199931639d56537d4a1939e498746
Kevin Liu <kevinliu314@gmail.com>
2018-11-10 15:52:29 -0600

    cleaning inputs

--- src/main.py
+++ src/main.py
@@ -8,8 +8,20 @@ script_file = utils.getFile('script_file.txt')
 input_board = []
 for line in field_file:
     input_board.append(list(line))
-
+steps = []
+for line in script_file:
+    steps.append(line.rstrip('\n').split(' '))
 my_board = board(input_board)
 
 print('Starting Board')
 my_board.printBoard()
+print('-------------')
+print('Starting Script')
+print('-------------')
+for step in steps:
+    print('step', step)
+    my_board.doStep(step)
+    my_board.printBoard()
+    print('Ship is at (', my_board.getShipPosition()['x'],', ',my_board.getShipPosition()['y'],')')
+    print('+++++++++')
+    # my_board.evalBoardState()
