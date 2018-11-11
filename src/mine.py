f1569b94808cae0e695ebf0e0d006004b7f41dd2
Kevin Liu <kevinliu314@gmail.com>
2018-11-10 19:55:03 -0600

    add reverse char converter

--- src/mine.py
+++ src/mine.py
@@ -10,10 +10,16 @@ class mine:
     def convertToDist(self):
         #since we can't use unicode conversion we just define our own dict to convert distances
         z = {}
-        for ch in range(65,91):
-            z[chr(ch)] = ch-38
         for ch in range(97, 123):
             z[chr(ch)] = ch-96
+        for ch in range(65,91):
+            z[chr(ch)] = ch-38
+
         return z[self.z_dist]
     def convertToChar(self):
         ch = {}
+        for val in range(1,27):
+             ch[val] = chr(val+96)
+        for val in range(27, 53):
+            ch[val] = chr(val+38)
+        self.z_dist = ch[self.dist]
